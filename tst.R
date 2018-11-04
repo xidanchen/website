687a848cd09fb4f07883e2627db72e0dce1e8998
xidan chen <xidan.chen@gmail.com>
2018-11-04 01:25:32 +0100

    change theme

---
+++ tst.R
@@ -0,0 +1,19 @@
+#luizdepra/hugo-coder
+blogdown::new_site(theme = 'luizdepra/hugo-coder')
+#https://www.quantifyhappiness.com/
+
+# [[params.social]]
+# name = "Github"
+# icon = "fab fa-github"
+# weight = 1
+# url = "https://github.com/xidanchen/"
+# [[params.social]]
+# name = "Twitter"
+# icon = "fab fa-twitter"
+# weight = 3
+# url = "https://twitter.com/XidanChen/"
+# [[params.social]]
+# name = "LinkedIn"
+# icon = "fab fa-linkedin"
+# weight = 4
+# url = "https://www.linkedin.com/in/xidan-amber-chen-9a538b20/"
